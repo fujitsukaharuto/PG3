678dd9bb0a70ed20e18ef9be0599ff4cd330a2a3
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2024-09-30 11:58:19 +0900

    確認課題01_01

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
+#include <stdio.h>
+#include <cctype>
 #include <iostream>
 #include <Windows.h>
 
+template <typename T>
+T add(T a, T b) {
+	return static_cast<T>(a + b);
+}
+
+template <typename T>
+T compar(T a, T b) {
+	return a <= b ? a : b;
+}
+
+template<>
+char compar<char>(char a, char b) {
+
+	std::cout << "”ŽšˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ" << std::endl;
+	return '\0';
+
+}
+
+
+
 int main() {
+
+	std::cout << compar<int>(1, 2) << std::endl;
+	std::cout << compar<float>(10.0f, 8.0f) << std::endl;
+	std::cout << compar<double>(0.01, 0.003) << std::endl;
+	std::cout << compar<char>('1', '2') << std::endl;
 
 	return 0;
 }
