b5cecc36cc16b516004204ff10ed72d05d32b515
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2024-11-25 10:08:50 +0900

    確認課題03_01

--- main.cpp
+++ main.cpp
@@ -1,7 +1,58 @@
 #include <iostream>
 #include <Windows.h>
+#include <list>
 
 int main() {
+
+	std::list<const char*> yamanoteLine{
+		"Tokyo","Kanda","Akihabara","Okachimachi","Ueno","Uguisudani",
+		"Nippori","Tabata","Komagome","Sugamo","Otsuka","Ikebukuro",
+		"Mejiro","Takadanobaba","Shin-Okubo","Shinjuku","Yoyogi",
+		"Harajuku","Shibuya","Ebisu","Meguro","Gotanda","Osaki","Shinagawa",
+		"Tamachi","Hamamatsucho","Shimbashi","Yurakucho"
+	};
+
+	std::cout << "1970”N" << std::endl;
+	for (std::list<const char*>::iterator itr = yamanoteLine.begin();
+		itr != yamanoteLine.end(); ++itr) {
+		std::cout << *itr << std::endl;
+	}
+
+
+	int JY = 1;
+	for (std::list<const char*>::iterator itr = yamanoteLine.begin();
+		itr != yamanoteLine.end(); ++itr) {
+		if (JY == 8) {
+			itr = yamanoteLine.insert(itr, "Nishi-Nippori");
+		}
+		JY++;
+	}
+
+	std::cout << std::endl;
+	std::cout << "2019”N" << std::endl;
+	for (std::list<const char*>::iterator itr = yamanoteLine.begin();
+		itr != yamanoteLine.end(); ++itr) {
+		std::cout << *itr << std::endl;
+	}
+
+
+	JY = 1;
+	for (std::list<const char*>::iterator itr = yamanoteLine.begin();
+		itr != yamanoteLine.end(); ++itr) {
+		if (JY == 26) {
+			itr = yamanoteLine.insert(itr, "Takanawa Gateway");
+		}
+		JY++;
+	}
+
+	std::cout << std::endl;
+	std::cout << "2022”N" << std::endl;
+	for (std::list<const char*>::iterator itr = yamanoteLine.begin();
+		itr != yamanoteLine.end(); ++itr) {
+		std::cout << *itr << std::endl;
+	}
+
+
 
 	return 0;
 }
