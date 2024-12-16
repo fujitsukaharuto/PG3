e1330d89765330b988e096f50c9a6e66216000ff
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2024-12-16 12:38:16 +0900

    確認課題06__02

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 #include <Windows.h>
+#include <thread>
+#include <functional>
 
 int main() {
+
+	int num = 1;
+
+	std::function<void()> RightTh = [&]() {
+		std::cout << "thread " << 1 << std::endl;
+		num++;
+		};
+
+	std::function<void()> RightTh2 = [&]() {
+		std::cout << "thread " << 2 << std::endl;
+		num++;
+		};
+
+	std::function<void()> RightTh3 = [&]() {
+		std::cout << "thread " << 3 << std::endl;
+		num++;
+		};
+
+
+	std::thread th1(RightTh);
+	th1.join();
+	std::thread th2(RightTh2);
+	th2.join();
+	std::thread th3(RightTh3);
+	th3.join();
 
 	return 0;
 }
